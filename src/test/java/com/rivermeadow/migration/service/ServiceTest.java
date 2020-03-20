196e7f0ee0c89a9aca1550206894ef6a51621b35
Ethan Cheng <cjs1234567@hotmail.com>
2020-03-19 20:12:36 -0400

    add test case.

--- src/test/java/com/rivermeadow/migration/service/ServiceTest.java
+++ src/test/java/com/rivermeadow/migration/service/ServiceTest.java
@@ -7,9 +7,10 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,47 +18,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.rivermeadow.migration.data.MigrationRepository;
 import com.rivermeadow.migration.model.Migration;
 import com.rivermeadow.migration.model.MigrationStatus;
+import com.rivermeadow.migration.model.TargetCloud;
 import com.rivermeadow.migration.model.Volume;
+import com.rivermeadow.migration.model.Workload;
+import com.rivermeadow.migration.model.WorkloadEmbaddable;
 import com.rivermeadow.migration.service.exception.MigrationAlreadyRun;
 
 @SpringBootTest
-@Disabled
+//@Disabled
+@RunWith(JUnitPlatform.class)
 public class ServiceTest {
 
 	@Mock
 	private MigrationRepository migrationRepository;
 
 	@InjectMocks // auto inject helloRepository
-	private MigrationService MigrationService = new MigrationService();;
+	private MigrationService migrationService = new MigrationService();
 
-	@BeforeEach
-	void setMockOutput() {
-		Migration migrationWithoutC = new Migration();
-		migrationWithoutC.setId(1L);
-		Set<Volume> volumeSet = new HashSet<>();
-		Volume v1 = new Volume();
-		v1.setMountPoint("D:\\");
-		v1.setSize(10L);
-		Volume v2 = new Volume();
-		v2.setMountPoint("F:\\");
-		v2.setSize(20L);
-		volumeSet.add(v1);
-		volumeSet.add(v2);
-
-		migrationWithoutC.setSelectedVolume(volumeSet);
-		
-		Optional<Migration> migrationOptional = Optional.of(migrationWithoutC);
-
-		when(migrationRepository.findById(1L)).thenReturn(migrationOptional);
-	}
 
 	@Test
-	public void testMigrationSelectedVolumeNoHaveC() throws MigrationAlreadyRun, InterruptedException {
+	public void testMigrationSelectedVolumeNoTHaveC() throws MigrationAlreadyRun, InterruptedException {
 
-		MigrationService MigrationService = new MigrationService();
-		
-//		MigrationRepository migrationRepository = mock(MigrationRepository.class);
-		
 		Migration migrationWithoutC = new Migration();
 		migrationWithoutC.setId(1L);
 		Set<Volume> volumeSet = new HashSet<>();
@@ -69,19 +50,68 @@ public class ServiceTest {
 		v2.setSize(20L);
 		volumeSet.add(v1);
 		volumeSet.add(v2);
-		
 
 		migrationWithoutC.setSelectedVolume(volumeSet);
 		migrationWithoutC.setMigrationStatus(MigrationStatus.NOTSTARTED);
 		
 		Optional<Migration> migrationOptional = Optional.of(migrationWithoutC);
-		
-		when(migrationRepository.findById(1L)).thenReturn(migrationOptional);
-		
-		MigrationService.setMigrationRepository(migrationRepository);
 
-		Migration m = MigrationService.runMigration(1L);
+		when(migrationRepository.findById(1L)).thenReturn(migrationOptional);
+
+		Migration m = migrationService.runMigration(1L);
 		
 		assertTrue(m.getMigrationStatus().toString().equals("ERROR"));
+	}
+	
+	@Disabled//it has a sleep inside of program. If you want to run, it will wait 1min to finish
+	@Test
+	public void testMigrationSelectedVolumeHaveC() throws MigrationAlreadyRun, InterruptedException {
+
+		Migration migrationWittC = new Migration();
+		migrationWittC.setId(1L);
+		Set<Volume> volumeSet = new HashSet<>();
+		Volume v1 = new Volume();
+		v1.setMountPoint("C:\\");
+		v1.setSize(10L);
+		Volume v2 = new Volume();
+		v2.setMountPoint("F:\\");
+		v2.setSize(20L);
+		volumeSet.add(v1);
+		volumeSet.add(v2);
+
+		migrationWittC.setSelectedVolume(volumeSet);
+		migrationWittC.setMigrationStatus(MigrationStatus.NOTSTARTED);
+		
+		Set<Volume> volumeSource = new HashSet<>();
+		Volume v3 = new Volume();
+		v3.setMountPoint("D:\\");
+		v3.setSize(10L);
+		Volume v4 = new Volume();
+		v4.setMountPoint("F:\\");
+		v4.setSize(20L);
+		volumeSource.add(v3);
+		volumeSource.add(v4);
+		
+		Workload wl = new Workload();
+		wl.setVolumeSet(volumeSource);
+		
+		System.out.println(wl);
+		
+		migrationWittC.setSource(wl);
+		
+		WorkloadEmbaddable wl2 = new WorkloadEmbaddable();
+		TargetCloud targetCloud = new TargetCloud();
+		targetCloud.setWorkload(wl2);
+		migrationWittC.setTarget(targetCloud);
+		
+		System.out.println(migrationWittC);
+		
+		Optional<Migration> migrationOptional = Optional.of(migrationWittC);
+
+		when(migrationRepository.findById(1L)).thenReturn(migrationOptional);
+
+		Migration m = migrationService.runMigration(1L);
+		
+		assertTrue(m.getMigrationStatus().toString().equals("SUCCESS"));
 	}
 }
